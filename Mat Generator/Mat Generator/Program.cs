f99b600de2ee907c952dfd6aaf31fb84905fc7fc
feli03002 <73816572+feli03002@users.noreply.github.com>
2021-02-08 10:56:11 +0100

    Add XML

---
+++ Mat Generator/Mat Generator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Mat_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // poopoopeepeetime
+
+            string path = System.IO.Path.Combine(Environment.CurrentDirectory, "..\\..\\Properties\\Resources\\Recept.xml");
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+
+            XmlNodeList xnList = doc.SelectNodes("/mat/recept [@Type=")            
+
+        }
+    }
+}
